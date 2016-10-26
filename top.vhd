1b7a2149100c8b63d22ea5a830eaf96a0704972e
Postaron <florent.denef@gmail.com>
2016-10-26 16:26:57 +0200

    commentaires supplémentaires

--- top.vhd
+++ top.vhd
@@ -6,31 +6,31 @@ use ieee.std_logic_arith.all;
 library work;
 use work.malib.all;
 ENTITY top IS PORT(
-	--entrées
+	--entrées physiques
 	clk50, boutonD, boutonG, sensorA, sensorB : in std_logic;
 	interD_mli, interG_mli : in std_logic_vector(3 downto 0);
-	--sorties
+	--sorties physiques
 	en_g, en_d, dirg, dird, sortieSensorA, sortieSensorB : out std_logic);
 END ENTITY;
 
 ARCHITECTURE toto OF top IS
-signal clk400k : std_logic;
-signal out_cpt : std_logic_vector(7 downto 0);
+signal clk400k : std_logic; --signal interne
+signal out_cpt : std_logic_vector(7 downto 0); --signal interne
 BEGIN
-	ic1 : dds PORT MAP (
-	clk50 => clk50, dn => X"020C49BA", Q => clk400k);
+	ic1 : dds PORT MAP ( --premier dds de 400kHz
+	clk50 => clk50, dn => X"020C49BA", Q => clk400k); --valeur trouvée par le calcul
 	
-	ic2 : cpt_mli PORT MAP (
+	ic2 : cpt_mli PORT MAP ( --compteur de PWM
 	clk400k => clk400k, Q => out_cpt);
 	
-	ic3 : comp_mli PORT MAP (
+	ic3 : comp_mli PORT MAP ( --comparateur PWM moteurG
 	clk => clk400k, val_mli => out_cpt , cons_mli(3 downto 0) => "1111", cons_mli(7 downto 4) => interG_mli, out_mli => en_g);
 	
-	ic4 : comp_mli PORT MAP (
+	ic4 : comp_mli PORT MAP ( --comparateur PWM moteurD
 	clk => clk400k, val_mli => out_cpt, cons_mli(3 downto 0) => "1111", cons_mli(7 downto 4) => interD_mli, out_mli => en_d);
 	
 	process(clk50) begin
-		if rising_edge(clk50) then
+		if rising_edge(clk50) then --quand front montant alors
 			if boutonG = '1' then
 				dirg <= '1';
 			else
@@ -44,6 +44,7 @@ BEGIN
 			
 		end if;
 	end process;
-	sortieSensorA <= sensorA;
-	sortieSensorB <= sensorB;
+	sortieSensorA <= sensorA; --on sort les valeurs des sensor
+	sortieSensorB <= sensorB; -- de façon combinatoire (en permanence)
+									  -- le compilo fait un simple fil de l'entrée vers la sortie
 END toto;
