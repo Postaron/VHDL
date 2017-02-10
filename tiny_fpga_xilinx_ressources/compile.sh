49e2a39a0f77bdbb1b85eb030f4ff2c9750d4aef
Postaron <Postaron@users.noreply.github.com>
2017-02-10 22:36:31 +0100

    no debug option (-g)

--- tiny_fpga_xilinx_ressources/compile.sh
+++ tiny_fpga_xilinx_ressources/compile.sh
@@ -3,8 +3,8 @@
 #export PATH=$PATH:/opt/Xilinx/14.5/ISE_DS/ISE/bin/lin64
 #export PATH=$PATH:/usr/bin:/opt/Xilinx/11.1/ISE/bin/lin
 cp ../microcontroleur.bit ./
-avr-gcc -g -mmcu=attiny861 -Wall -Os -c linefollow1.c
-avr-gcc -g -mmcu=attiny861 -o linefollow1.elf -Wl,-Map,linefollow1.map linefollow1.o
+avr-gcc -mmcu=attiny861 -Wall -Os -c linefollow1.c
+avr-gcc -mmcu=attiny861 -o linefollow1.elf -Wl,-Map,linefollow1.map linefollow1.o
 avr-objdump -h -S linefollow1.elf > linefollow1.lss
 data2mem -bm attiny861.bmm -bd linefollow1.elf -bt microcontroleur.bit -o uh microcontroleur
 #djtgcfg prog -d Nexys3 --index 0 --file microcontroleur_rp.bit
